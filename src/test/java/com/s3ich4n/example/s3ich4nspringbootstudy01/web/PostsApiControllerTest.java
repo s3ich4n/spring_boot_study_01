041d53bd9b80f25d9a31e829bcbeab20491c5b47
s3ich4n <s3ich4n@gmail.com>
2024-05-18 15:53:09 +0900

    feat: 생성기능 만들기

--- src/test/java/com/s3ich4n/example/s3ich4nspringbootstudy01/web/PostsApiControllerTest.java
+++ src/test/java/com/s3ich4n/example/s3ich4nspringbootstudy01/web/PostsApiControllerTest.java
@@ -1,16 +1,68 @@
 package com.s3ich4n.example.s3ich4nspringbootstudy01.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.s3ich4n.example.s3ich4nspringbootstudy01.domain.Posts;
+import com.s3ich4n.example.s3ich4nspringbootstudy01.domain.PostsRepository;
+import com.s3ich4n.example.s3ich4nspringbootstudy01.web.dto.PostsSaveRequestDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+/**
+ * JPA 기능까지 한번에 테스트하려면 이런 식으로.
+ */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// @WebMvcTest(
-//     controllers = PostsApiController.class,
-//     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-// )
 public class PostsApiControllerTest {
     
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록여부확인() throws Exception {
+        // given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                                                            .title(title)
+                                                            .content(content)
+                                                            .author("author")
+                                                            .build();
+
+        // 더 깔끔한 방법 없을까
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
 }
